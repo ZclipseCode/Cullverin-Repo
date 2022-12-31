d373f52902fbda6216009c66512b7346e0aeba1c
ZclipseCode <bmichaelbusiness@gmail.com>
2022-12-31 17:51:18 -0600

    rpg explosion work

---
+++ Assets/Scripts/Guns/RPG/DestroyRocket.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyRocket : MonoBehaviour
+{
+    [SerializeField] GameObject explosion;
+    [SerializeField] float deleteTime;
+    float currentTime = 0;
+    Transform rocketTrail;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        rocketTrail = GameObject.FindGameObjectWithTag("Rocket Trail").GetComponent<Transform>();
+
+        currentTime += Time.deltaTime;
+
+        if (currentTime >= deleteTime)
+        {
+            Explosion();
+        }
+    }
+
+    void Explosion()
+    {
+        Instantiate(explosion, rocketTrail.transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground" || collision.gameObject.tag == "Enemy")
+        {
+            Explosion();
+        }
+    }
+}
